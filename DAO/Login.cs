aff673636ad4e0fd7e6a6b2d0d16dec3190f617c
Alina081 <tanjinaalina777@gmail.com>
2024-10-04 23:23:00 +0100

    first commit

---
+++ DAO/Login.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    [Table(name: "Login")]
+    [Serializable]
+   public class Login
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Enter Username")]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        public string Password { get; set; }
+
+        public int RoleId { get; set; }
+
+    }
+}
---
+++ DAO/Profile.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+   
+    [Table(name: "Profile")]
+      [Serializable]
+    public  class Profile
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Required")]
+        public string UserName { get; set; }
+
+       
+
+        [Required(ErrorMessage = "Required")]
+        public string Address { get; set; }
+
+        
+
+        [Required(ErrorMessage = "Required")]
+        [Phone]
+        public string Pnumber { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+      
+
+        [Required(ErrorMessage = "Required")]
+        public string ProfileImage { get; set; }
+
+
+
+        [Required(ErrorMessage = "Required")]
+        public string AccountType { get; set; }
+
+
+        [Required(ErrorMessage = "Required")]
+        public string AccountNumber { get; set; }
+
+
+
+
+    }
+}
